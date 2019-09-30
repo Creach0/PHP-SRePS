36e474398eb8746ebffcb822f113e6aa02155299
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-09-30 16:25:53 +1000

    Added a php file storing the database connection information

---
+++ settings.php
@@ -0,0 +1,6 @@
+<?php
+  $host = "feenix-mariadb.swin.edu.au"; // host
+  $user = "s102118895";                 // username
+  $pwd = "mysqlpassword";               // password
+  $dbnm = "s102118895_db";              // database
+?>
