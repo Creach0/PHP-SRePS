cb8c867b03e38b5114433e779b26f59c0ed85beb
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-09-30 22:26:29 +1000

    Added report file that can calculate the start date given the end date and length

---
+++ reportdisplay.php
@@ -0,0 +1,35 @@
+<?php
+  include_once("settings.php");
+
+  //$conn = mysqli_connect($host, $user, $pwd, $database) or die ('Failed to connect to the database');
+
+  $date_in_seconds = strtotime($_POST['end_date']);
+  $end_date = date('d/m/y', $date_in_seconds);
+  $length = $_POST['length'];
+
+  switch($length)
+  {
+    case "week":
+      // gets the date a week before
+      $start_date = date('d/m/y', $date_in_seconds - 604800);
+      break;
+    case "month":
+      // gets the date a month before
+      $start_date = date('d/m/y', $date_in_seconds - 2629746);
+      break;
+  }
+
+  echo $start_date . "   " . $end_date;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>SRePS</title>
+	</head>
+
+	<body>
+		<h1>Generated Report</h1>
+    
+	</body>
+</html>
