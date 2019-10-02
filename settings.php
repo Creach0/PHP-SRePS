043afe5439f01d3ce1fb2a8f59d87919ab6697b1
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-10-02 22:58:54 +1000

    Changed settings file to reflect the new database details

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
-  $host = "feenix-mariadb.swin.edu.au"; // host
-  $user = "s102118895";                 // username
-  $pwd = "mysqlpassword";               // password
-  $dbnm = "s102118895_db";              // database
+  $host = "localhost";    // host
+  $user = "php_user";     // username
+  $pwd  = "php_password"; // password
+  $dbnm = "db2";          // database
 ?>
