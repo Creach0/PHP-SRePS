6eb08fdf0693ee94c7c6a53f3f361bb4eef87e6d
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-10-03 19:35:36 +1000

    Added table displays for total sales per date and amount left and total sales per product

--- reportdisplay.php
+++ reportdisplay.php
@@ -1,7 +1,7 @@
 <?php
   include_once("settings.php");
 
-  //$conn = mysqli_connect($host, $user, $pwd, $database) or die ('Failed to connect to the database');
+  $conn = @mysqli_connect($host, $user, $pwd, $database) or die ('Failed to connect to the database');
 
   $date_in_seconds = strtotime($_POST['end_date']);
   $end_date = date('Y-m-d', $date_in_seconds);
@@ -32,12 +32,12 @@
 
         <h3>Sales by category</h3>
         <table>
-            <tr> <th>Category</th> <th>Sales Price</th> </tr>
+            <tr> <th>Category</th> <th>Total Sales</th> </tr>
             <?php
-                $query  = "SELECT Category.CategoryName, (Sales.Price * Sales.Quantity) AS Total FROM Category NATURAL JOIN Products NATURAL JOIN Sales 
+                $query  = "SELECT Category.CategoryName, (Sales.Price * Sales.Quantity), Sales.Date AS Total FROM Category NATURAL JOIN Products NATURAL JOIN Sales 
                     WHERE Sales.Date > '$start_date' AND Sales.Date < '$end_date' GROUP BY Category.CategoryName";
 
-                $result = @mysqli_query($conn, $query);
+                $result = mysqli_query($conn, $query);
 
                 while ($row = $result->fetch_assoc())
                 {
@@ -50,8 +50,43 @@
         </table>
 
         <h3>Sales by date</h3>
+        <table>
+            <tr> <th>Date</th> <th>Total Sales</th> </tr>
+            <?php
+                $query = "SELECT Date, (Price * Quantity) AS Total FROM Category NATURAL JOIN Products NATURAL JOIN Sales 
+                        WHERE Date > '$start_date' AND Date < '$end_date' GROUP BY Date";
+
+                $result = mysqli_query($conn, $query);
+
+                while ($row = $result->fetch_assoc())
+                {
+                    echo "<tr>"
+                        ."<td>".$row["Date"]."</td>"
+                        ."<td>".$row["Total"]."</td>"
+                        ."</tr>";
+                }
+            ?>
+        </table>
 
         <h3>Remaining product stocks</h3>
+        <table>
+            <tr> <th>Product Name</th> <th>Amount Left</th> <th>Amount Sold</th> </tr>
+            <?php
+                $query = "SELECT Date, (Price * Quantity) AS TotalSold, Stock, ProductName FROM Products NATURAL JOIN Sales 
+                            WHERE Date > '$start_date' AND Date < '$end_date' GROUP BY ProductName";
+
+                $result = mysqli_query($conn, $query);
+
+                while ($row = $result->fetch_assoc())
+                {
+                    echo "<tr>"
+                        ."<td>".$row["ProductName"]."</td>"
+                        ."<td>".$row["Stock"]."</td>"
+                        ."<td>".$row["TotalSold"]."</td>"
+                        ."</tr>";
+                }
+            ?>
+        </table>
 
 	</body>
 </html>
