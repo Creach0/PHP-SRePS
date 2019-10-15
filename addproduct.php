8a432533f2327266f667da1745e6ef67ee513e22
LACHLAN WESTLAKE <102109556@student.swin.edu.au>
2019-10-15 21:36:09 +1100

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -134,3 +134,42 @@
 
 	</body>
 </html> --!>
+</form>
+
+
+
+<?php
+	try {
+			// Connect to database
+			require_once ("settings.php");
+			$conn = new mysqli($host,$user,$pwd,$dbnm);
+			if ($conn->connect_error) throw new Exception("Failed to connect to database: ".$conn->connect_error);
+
+			echo "<p>Connected to database.</p>";
+
+			$product = $_POST['product'];
+			$quantity = $_POST['quantity'];
+
+			//print_r( $_POST );
+			//$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES (1, 5, 9, '2019-10-3')";
+			$sql = "INSERT INTO Products (ProductName,CategoryId, Stock) VALUES(\"$product\",1, $quantity)";
+			//$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES((SELECT ProductId FROM Product WHERE ProductName = '$product'),'$quantity','$price','$date')";
+			//$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES (1, 3, 5, '2019-10-3')";
+			$result = mysqli_query($conn, $sql);
+
+			//$sql = "SELECT * FROM Sales;";
+			//$result = mysqli_query($conn, $sql);
+			//$resultCheck = mysqli_num_rows($result);
+
+						// Close everything
+				$conn->close();
+
+				} catch(Exception $e) {
+						echo "Oops! Something went wrong: ".$e->getMessage();
+				}
+				?>
+
+</section>
+
+</body>
+</html> --!>
