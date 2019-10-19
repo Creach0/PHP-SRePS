7679c30e4e99b4150756199304f2c9654c257971
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-10-19 15:01:02 +1100

    Alerts are now replaced by confirm windows which redirect to the stock levels page

--- scripts/alertChecker.js
+++ scripts/alertChecker.js
@@ -3,8 +3,9 @@ var alertShown = false;
 window.setInterval(function () {
     $.post('ajax/alert.php', {}, function(data) {
         if (!alertShown && data != "") {
-            alert(data);
             alertShown = true;
+            if (window.confirm(data + "\n Click \"ok\" to be redirected to the stock page to view the low stock items"))
+                window.location.href = '../stocklevels.php';
         }
     });
 }, 5000);
