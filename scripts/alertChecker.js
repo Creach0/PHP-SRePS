bdae456a12c838837d4ccf5aedea5db0bca12bde
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-10-19 14:46:06 +1100

    Updated the alert so that it is only displayed once per page rather than every interval

--- scripts/alertChecker.js
+++ scripts/alertChecker.js
@@ -1,5 +1,10 @@
+var alertShown = false;
+
 window.setInterval(function () {
     $.post('ajax/alert.php', {}, function(data) {
-        alert(data);
+        if (!alertShown) {
+            alert(data);
+            alertShown = true;
+        }
     });
 }, 5000);
