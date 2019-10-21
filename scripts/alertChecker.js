dfce9104005cca607f2409e2c5b01b3b0954de49
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-10-21 23:17:55 +1100

    JavaScript now displays alerts as inner html in a 'stockAlert' div

--- scripts/alertChecker.js
+++ scripts/alertChecker.js
@@ -4,10 +4,11 @@ function SetCookie(){
 
 window.setInterval(function () {
     $.post('ajax/alert.php', {}, function(data) {
-        if (data != "" && document.cookie.indexOf("timer") === -1) {
-            SetCookie();
-            if (window.confirm(data + "\n Click \"ok\" to be redirected to the stock page to view the low stock items"))
-                window.location.href = '../stocklevels.php';
+        if (data != "") {
+            document.getElementById("stockAlert").style.display = "block";
+            document.getElementById("stockAlert").innerHTML = data;
         }
+        else
+            document.getElementById("stockAlert").style.display = "none";
     });
 }, 5000);
