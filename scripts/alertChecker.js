2447c466e0223659c92226477a2c625c67aed53d
Jeremy <jeremy.v.maxwell@bigpond.com>
2019-10-21 23:03:11 +1100

    Added cookies so alerts aren't spammed as much

--- scripts/alertChecker.js
+++ scripts/alertChecker.js
@@ -1,9 +1,11 @@
-var alertShown = false;
+function SetCookie(){
+    document.cookie = "timer=1; max-age=300; path=\\";
+}
 
 window.setInterval(function () {
     $.post('ajax/alert.php', {}, function(data) {
-        if (!alertShown && data != "") {
-            alertShown = true;
+        if (data != "" && document.cookie.indexOf("timer") === -1) {
+            SetCookie();
             if (window.confirm(data + "\n Click \"ok\" to be redirected to the stock page to view the low stock items"))
                 window.location.href = '../stocklevels.php';
         }
