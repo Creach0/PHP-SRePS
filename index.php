e00d3ba5a936235c09caed053bce587ee0049aaf
Pestery <mriley.au@gmail.com>
2019-10-22 22:33:35 +1100

    Add index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+	include_once("common.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<?php echo_head(); ?>
+	<body>
+		<header>
+			<h1>PHP-SRPS Home</h1>
+		</header>
+		<?php echo_nav(); ?>
+        <p>This website is for a group university project. If by some chance you’ve found yourself here then feel free to have a look around but please don’t destroy anything.</p>
+        <div id="stockAlert"></div>
+	</body>
+</html>
