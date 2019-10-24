a1ce5024b9ace237cf0003ff4a76fba5937be99f
Pestery <mriley.au@gmail.com>
2019-10-25 00:03:43 +1100

    Changed addrecord.php to try and reduce the number of affected lines during merge

--- addrecord.php
+++ addrecord.php
@@ -9,7 +9,6 @@
 	<head>
 		<script src="scripts/AddRecordValidateInput.js"></script>
 	</head>
-
 	<body>
 		<header>
 			<h1>Sales Records</h1>
@@ -32,7 +31,8 @@
 				<label for="date">Date:
 				<input type="date" name="date" id="date" /></label><br/>
 
-				<input type="hidden" id="inputValid" name="inputValid"/>
+                <input type="hidden" id="inputValid" name="inputValid"/>
+
 				<p>
 					<input type="submit" value="Add" onclick="return validateInput()" />
 					<input type="reset" value="Clear" />
@@ -41,31 +41,42 @@
 			</form>
 
 			<?php
-				if ($_POST['inputValid'] = "true") {
-					try {
-							// Connect to database
-							require_once ("settings.php");
-							$conn = new mysqli($host,$user,$pwd,$dbnm);
-							if ($conn->connect_error) throw new Exception("Failed to connect to database: ".$conn->connect_error);
+				try {
 
-							$product = $_POST['product'];
-							$quantity = $_POST['quantity'];
-							$price = $_POST['price'];
+						// Connect to database
+						require_once ("settings.php");
+						$conn = new mysqli($host,$user,$pwd,$dbnm);
+						if ($conn->connect_error) throw new Exception("Failed to connect to database: ".$conn->connect_error);
 
-							$date_in_seconds = strtotime($_POST['date']);
-							$date = date('Y-m-d', $date_in_seconds);
+						echo "<p>Connected to database.</p>";
 
-							$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES((SELECT ProductId FROM Products WHERE ProductName = \"$product\"),\"$price\",\"$quantity\",\"$date\")";
-							$result = mysqli_query($conn, $sql);
 
-							// Close everything
+						$product = $_POST['product'];
+						$quantity = $_POST['quantity'];
+						$price = $_POST['price'];
+
+
+						$date_in_seconds = strtotime($_POST['date']);
+						$date = date('Y-m-d', $date_in_seconds);
+
+						//print_r( $_POST );
+						//$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES (1, 5, 9, '2019-10-3')";
+						$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES((SELECT ProductId FROM Products WHERE ProductName = \"$product\"),\"$price\",\"$quantity\",\"$date\")";
+						//$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES((SELECT ProductId FROM Product WHERE ProductName = '$product'),'$quantity','$price','$date')";
+						//$sql = "INSERT INTO Sales (ProductId, Price, Quantity, Date) VALUES (1, 3, 5, '2019-10-3')";
+						$result = mysqli_query($conn, $sql);
+
+						//$sql = "SELECT * FROM Sales;";
+						//$result = mysqli_query($conn, $sql);
+						//$resultCheck = mysqli_num_rows($result);
+
+									// Close everything
 							$conn->close();
 
 							} catch(Exception $e) {
 									echo "Oops! Something went wrong: ".$e->getMessage();
 							}
-                        }
-                    ?>
+							?>
             <div id="stockAlert"></div>
 
 		</section>
